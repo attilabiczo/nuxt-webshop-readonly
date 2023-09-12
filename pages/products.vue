d5f819e4e8c759ad66aa750828831fd0207a82b0
Attila Biczo <attila.biczo@hu.fressnapf.eu>
2023-09-12 22:14:11 +0200

    Fixed listing products

--- pages/products.vue
+++ pages/products.vue
@@ -3,8 +3,10 @@
     <Breadcrumb />
     <h1 class="text-3xl mb-6">Products</h1>
     <ProductListTemplate :is-loading="productStore.isLoading" :has-error="productStore.hasError">
-      <ProductItem class="flex-[1_1_50%] max-w-[50%] sm:flex-[1_1_33%] sm:max-w-[33%] md:flex-[1_1_25%] md:max-w-[25%] lg:flex-[1_1_20%] lg:max-w-[20%]"
+      <div class="flex flex-wrap">
+        <ProductItem class="flex-[1_1_50%] max-w-[50%] sm:flex-[1_1_33%] sm:max-w-[33%] md:flex-[1_1_25%] md:max-w-[25%] lg:flex-[1_1_20%] lg:max-w-[20%]"
                    v-if="!productStore.isLoading" :product="product" v-for="product in productStore.products" :key="product.id" />
+      </div>
     </ProductListTemplate>
   </PageContainer>
 </template>
